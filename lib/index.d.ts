c3727499ca666f0d6cde77748dda333d6da9c94e
Kasimir Pihlasviita <kasimir@pihlasviita.fi>
2025-04-14 10:52:04 +0300

    Order date utils alphabetically

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,37 +1,37 @@
 declare const ProtoUtils: {
     currency: (number: number, hideCents: boolean) => string;
     date: {
-        dayInMS: number;
-        today: Date;
-        tomorrow: Date;
-        yesterday: Date;
         ISO: {
             today: string;
             tomorrow: string;
             yesterday: string;
         };
+        ISO2Local: (string: string) => string;
+        ISO2LocalRange: (dateObj: {
+            startDate: string;
+            endDate: string;
+        }) => string;
+        ISO2LocalShortDay: (string: string) => string;
+        ISO2LocalShortMonth: (string: string) => string;
+        addDaysToDate: (baseDate: Date | string, days: number, format?: "ISO" | "local" | "data") => string | Date;
         current: {
             year: number;
             month: number;
             day: number;
         };
-        isISO: (string: string) => boolean;
         date2ISO: (date: Date) => string;
         date2Local: (date: Date) => string;
-        date2LocalShortMonth: (date: Date) => string;
         date2LocalShortDay: (date: Date) => string;
+        date2LocalShortMonth: (date: Date) => string;
         dateFromToday: (daysFromToday: number, format?: "ISO" | "local" | "data") => string | Date;
-        addDaysToDate: (baseDate: Date | string, days: number, format?: "ISO" | "local" | "data") => string | Date;
-        ISO2Local: (string: string) => string;
-        ISO2LocalShortMonth: (string: string) => string;
-        ISO2LocalShortDay: (string: string) => string;
-        ISO2LocalRange: (dateObj: {
-            startDate: string;
-            endDate: string;
-        }) => string;
+        dayInMS: number;
+        isISO: (string: string) => boolean;
         local2ISO: (string: string) => string;
         string2ISO: (string: string) => string;
         string2Local: (string: string) => string;
+        today: Date;
+        tomorrow: Date;
+        yesterday: Date;
     };
     downloadFile: (url: string, fileName?: string) => void;
     id: (prefix?: string) => string;
